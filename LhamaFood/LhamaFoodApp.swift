e3cf5b44a99749f1833d259c20f1051db2712e25
Agronault <xande.luis@hotmail.com>
2024-08-18 10:24:59 -0300

    Initial Commit

---
+++ LhamaFood/LhamaFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  LhamaFoodApp.swift
+//  LhamaFood
+//
+//  Created by Alexandre Martins on 18/08/24.
+//
+
+import SwiftUI
+
+@main
+struct LhamaFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
